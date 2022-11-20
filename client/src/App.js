36c86db29192ef787a835b7b061cbc988c2ef601
Marcus G. Oware <90935734+Weten-Ogen@users.noreply.github.com>
2022-11-20 13:57:44 +0000

    react app

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ChatEngine } from 'react-chat-engine';
+
+const App = () => {
+  return(
+    <ChatEngine
+    height="100vh"
+    projectID = ""
+    userName =""
+    userSecret=""
+    />
+  );
+}
+export default App;
