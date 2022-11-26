160105d153950c302c22b92bc38c6ebd19079eb2
Weten-Ogen <alphaoware@gmail.com>
2022-11-26 12:01:14 +0000

    learned about styling in react components

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,28 @@
 import React from 'react';
 
 
-const App = () => {
+
+
+const Helloworld = (props) => {
+  const HelloStyle = {
+    padding: 10,
+    fontSize:30,
+
+  
+  }
+  return (
+    
+    <div style={HelloStyle}>
+    Hello World to you {props.person}
+    </div>
+  );
+}
+
+function App() {
   return(
     <div>
+      <Helloworld person="Marcus"/>
+      <Helloworld person="Veronica"/>
     </div>
   );
 }
