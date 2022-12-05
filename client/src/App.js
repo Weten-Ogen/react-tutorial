a12bb982ea7afc890d908145d8b9c1572ae0af8a
Weten-Ogen <alphaoware@gmail.com>
2022-12-05 21:14:19 +0000

    worked on the page i was building

--- client/src/App.js
+++ client/src/App.js
@@ -1,68 +1,30 @@
 import React from 'react';
+import './Header.css';
 
 
 function App() {
-  const AppStyle = {
-    display:'flex',
-    flexWrap:'wrap',
-  }
+  
   return(
-    <div style={AppStyle}>
-      <Bg color ={'red'}/>
-      <Bg/>
-      <Bg/>
-      <Bg/>
-      <Bg/>
-      <Bg/>
-    
+    <div id='mainCont'>
+      <Header/>
     </div>
-  );
+  )
 }
 export default App;
 
-const Bg = (props) => {
-  const BgStyle= {
-    margin: '1rem',
-    padding: '0px',
-    width: '27%',
-    height:'55vh',
-    backgroundColor:'white',
-    boxShadow:"1px 1px 10px 1px black",
-  }
+
+const Header = (props) => {
+  
   return (
-    <div 
-    style={BgStyle}
-    >
-    <Square/>
-    <Label/>
-
-
+    <div id='container'>
+      <h1>Welcome</h1>
+      <ul className='navBar'>
+        <li><a href="#pricing" className='navItem'>Pricing</a></li>
+        <li><a href="#about" className='navItem'>About</a></li>
+        <li><a href="#contact" className='navItem'>Contact</a></li>
+      </ul>
     </div>
-  )
-}
-const Square = () => {
-  const SquareStyle = {
-    height:"40vh",
-    backgroundColor:'red',
-  }
-  return (
-    <div style={SquareStyle}>
-    </div>
-  )
-}
-
-const Label = () => {
-  const LabelStyle = {
-    textAlign: 'center',
-    fontSize : '24px',
-  }
-  return (
-    <div style={LabelStyle}>
-      <p>#0000EF</p>
-     
     
-    </div>
   )
-}
-
+};
 
