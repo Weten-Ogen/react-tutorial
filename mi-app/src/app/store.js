5260a133c2863ad8ef7fa38f1edbda9aa607669b
Weten-Ogen <alphaoware@gmail.com>
2023-01-20 12:25:09 +0000

    worked on redux/toolkit slices and stores

--- mi-app/src/app/store.js
+++ mi-app/src/app/store.js
@@ -1,8 +1,9 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import cartReducer  from './features/cart'
+
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-});
+     reducer:{
+        cart: cartReducer,
+    },
+})
